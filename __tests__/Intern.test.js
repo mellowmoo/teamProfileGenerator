809c1e41cde207bd9bda8fec1b4e45301db373bc
Muhaimen Ahmed <muhaimen.ahmed@gmail.com>
2021-10-28 16:54:32 -0400

    wrote Intern.test.js, passes all tests

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,23 @@
+// setup intern dependecy
+const Intern = require("../lib/Intern");
+
+// test setting school via constructor
+test("Can set school via constructor", () => {
+  const testValue = "Baruch College";
+  const e = new Intern("Moo", 1, "test@test.com", testValue);
+  expect(e.school).toBe(testValue);
+});
+
+// test getRole() in intern
+test("getRole() should return \"Intern\"", () => {
+  const testValue = "Intern";
+  const e = new Intern("Moo", 1, "test@test.com", "Baruch College");
+  expect(e.getRole()).toBe(testValue);
+});
+
+// test getSchool()
+test("Can get school via getSchool()", () => {
+  const testValue = "Baruch College";
+  const e = new Intern("Moo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
